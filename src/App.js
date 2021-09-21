b5e086682b541ff6efbd46b035165dce436b2329
MichaelZane <stevensmike36@yahoo.com>
2021-09-21 13:17:39 -0500

    added mediaquery for main title

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import "./App.css";
 import "./components/picCard.css";
 import PicContainer from "./components/PicContainer";
 
-
 function App() {
   return (
     <>
-      <h1 className="main-title">Spacestagram </h1>
-      <p className="main-subtitle">
-        powered by Nasa's astronomy picture of the day API
-      </p>
-      <div className="link-wrapper">
-        <b id="link-github"><a href="https://github.com/MichaelZane/photoOfDay">See My Github Repo</a></b>
+      <div className="title-wrapper">
+        <h1 className="main-title">Spacestagram </h1>
+
+        <p className="main-subtitle">
+          powered by Nasa's astronomy picture of the day API
+        </p>
       </div>
-      
+      <div className="link-wrapper">
+        <b id="link-github">
+          <a href="https://github.com/MichaelZane/photoOfDay">
+            See My Github Repo
+          </a>
+        </b>
+      </div>
+
       <PicContainer />
     </>
   );
